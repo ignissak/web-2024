cc6397b04d028b7ef9d7c2c20ce97244950814ce
Jakub Bordáš <methalicity@gmail.com>
2024-01-29 13:24:28 +0100

    fix: not aligned experience heading

--- src/components/TimedExperience.svelte
+++ src/components/TimedExperience.svelte
@@ -9,7 +9,7 @@
 
 <div class="flex flex-col sm:flex-row gap-0 sm:gap-4">
 	<p class="flex-none w-25">{from} - {to ? to : 'present'}</p>
-	<div>
+	<div class="flex flex-col">
 		<a href={url} target="_blank">{title}</a>
 		<p class="my-1">{paragraph}</p>
 		<p class="muted">{skills.join(' • ')}</p>
